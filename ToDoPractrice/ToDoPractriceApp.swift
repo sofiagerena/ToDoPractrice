725fa87673ea98f68ad387af17b21405c0dc6e91
sofia <sofigerenacortes@gmail.com>
2023-06-28 09:47:57 -0500

    Initial Commit

---
+++ ToDoPractrice/ToDoPractriceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoPractriceApp.swift
+//  ToDoPractrice
+//
+//  Created by Monitor #5 on 28/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoPractriceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
